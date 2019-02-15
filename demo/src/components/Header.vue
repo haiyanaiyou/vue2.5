75569f38af0490c09a70c18ed04764fdc7c3132f
haiyanaiyou <906325802@qq.com>
2019-02-15 17:35:16 +0800

    add iconfont

---
+++ demo/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="iconfont">&#xe624;</span>
+        </div>
+        <div class="header-input">
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">城市</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+     .header
+        display: flex
+        line-height: 0.86rem
+        background: #00bcd4
+        color:#fff
+
+        .header-left
+            width:0.64rem
+            float:left
+        .header-input
+            flex:1
+            background : #ffffff
+            border-radius : .1rem
+            margin-top : .12rem
+            height : .64rem
+            line-height : .64rem
+            color: #ccc
+            margin-left: .2rem
+        .header-right
+            width: 1.24rem
+            float: right
+            text-align: center
+
+</style>
--- demo/src/pages/Home.vue
+++ demo/src/pages/Home.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        home
+        <v-header></v-header>
     </div>
 </template>
 
 <script>
+import VHeader from './../components/Header.vue'
 export default {
+    components:{ VHeader }
     
 }
 </script>
