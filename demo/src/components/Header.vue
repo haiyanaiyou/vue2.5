7228210a1f77474df6d54dd40ba7b1d9e6977052
haiyanaiyou <906325802@qq.com>
2019-02-15 18:05:14 +0800

    header

--- demo/src/components/Header.vue
+++ demo/src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="header">
         <div class="header-left">
-            <span class="iconfont">&#xe624;</span>
+            <span class="iconfont back-icon">&#xe624;</span>
         </div>
         <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
         </div>
-        <div class="header-right">城市</div>
+        <div class="header-right">城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -21,20 +24,25 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    @import '~style/varibles.styl'
      .header
         display: flex
         line-height: 0.86rem
-        background: #00bcd4
+        background: $bgColor
         color:#fff
 
         .header-left
             width:0.64rem
             float:left
+            .back-icon
+                text-align: center
+                font-size: 0.4rem
         .header-input
             flex:1
             background : #ffffff
             border-radius : .1rem
             margin-top : .12rem
+            padding-left:.2rem
             height : .64rem
             line-height : .64rem
             color: #ccc
@@ -43,5 +51,8 @@ export default {
             width: 1.24rem
             float: right
             text-align: center
+            .arrow-icon
+                font-size: .24rem
+                margin-left : -.04rem
 
 </style>
